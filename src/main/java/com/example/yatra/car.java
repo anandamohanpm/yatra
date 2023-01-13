1ba40ff6ee0ac256ab93fbc2c7dbc441a01d6f4d
anandamohanpm <anandamohanpm@gmail.com>
2023-01-13 12:02:30 +0530

    CodeRefactor

---
+++ src/main/java/com/example/yatra/car.java
@@ -0,0 +1,14 @@
+package com.example.yatra;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class car {
+    @GetMapping("/mycar")
+    public String getData(){
+        return "Please book your car, go anywhere";
+    }
+
+}
